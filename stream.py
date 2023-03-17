de0fde13c4397f356cb2e856f66eae6211e49864
Max Braun <max.braun@gmail.com>
2023-03-16 17:32:39 -0700

    Streaming for continuous transcription

--- stream.py
+++ stream.py
@@ -1,8 +1,12 @@
 from absl import app
 from absl import flags
 from absl import logging
+from functools import partial
+from functools import wraps
 import numpy as np
+import queue
 import sounddevice as sd
+from time import time as now
 import whisper
 
 FLAGS = flags.FLAGS
@@ -16,39 +20,82 @@ flags.DEFINE_integer('sample_rate', 16000,
                      'The sample rate of the recorded audio.')
 flags.DEFINE_integer('num_channels', 1,
                      'The number of channels of the recorded audio.')
-flags.DEFINE_integer('chunk_seconds', 30,
+flags.DEFINE_integer('channel_index', 0,
+                     'The index of the channel to use for transcription.')
+flags.DEFINE_integer('chunk_seconds', 10,
                      'The length in seconds of each recorded chunk of audio.')
+flags.DEFINE_string('latency', 'low', 'The latency of the recording stream.')
+
+
+# A decorator to log the timing of performance-critical functions.
+def timed(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = now()
+        result = func(*args, **kwargs)
+        stop = now()
+        logging.debug(f'{func.__name__} took {stop-start:.3f}s')
+        return result
+    return wrapper
+
+
+@timed
+def transcribe(model, audio):
+    # Run the Whisper model to transcribe the audio chunk.
+    result = whisper.transcribe(model=model, audio=audio)
+
+    # TODO: Process overlapping chunks of audio?
+
+    # Use the transcribed text.
+    text = result['text'].strip()
+    logging.info(text)
+
+
+@timed
+def stream_callback(indata, frames, time, status, audio_queue):
+    if status:
+        logging.warning(f'Stream callback status: {status}')
+
+    # Add this chunk of audio to the queue.
+    audio = indata[:, FLAGS.channel_index].copy()
+    audio_queue.put(audio)
+
+
+@timed
+def process_audio(audio_queue, model):
+    # Block until the next chunk of audio is available on the queue.
+    audio = audio_queue.get()
+
+    # Transcribe the latest audio chunk.
+    transcribe(model=model, audio=audio)
 
 
 def main(argv):
-    # Download the Whisper model into memory.
-    logging.info(f'Loading model "{FLAGS.model_name}" ...')
+    # Load the Whisper model into memory, downloading first if necessary.
+    logging.info(f'Loading model "{FLAGS.model_name}"...')
     model = whisper.load_model(name=FLAGS.model_name)
 
-    # Allocate a buffer for chunks of audio to be recorded into.
-    audio = np.zeros(
-        [FLAGS.chunk_seconds * FLAGS.sample_rate, FLAGS.num_channels],
-        dtype=np.float32)
+    # The first run of the model is slow (buffer init), so run it once empty.
+    logging.info('Warming model up...')
+    block_size = FLAGS.chunk_seconds * FLAGS.sample_rate
+    whisper.transcribe(model=model,
+                       audio=np.zeros(block_size, dtype=np.float32))
 
-    # Run the model once on empty audio to warm it up.
-    logging.info('Warming up the model ...')
-    whisper.transcribe(model=model, audio=audio.squeeze())
-
-    # TODO: Use sounddevice.InputStream
-    # TODO: Use overlapping chunks
-
-    # Record a chunk of audio.
-    logging.info('Recording audio ...')
-    sd.rec(device=FLAGS.input_device, samplerate=FLAGS.sample_rate,
-           out=audio, blocking=True)
-
-    # Transcribe the recorded audio.
-    logging.info('Transcribing audio ...')
-    result = whisper.transcribe(model=model, audio=audio.squeeze())
-    text = result['text'].strip()
-
-    # Display the transcribed text.
-    logging.info(text)
+    # Stream audio chunks into a queue and process them from there. The
+    # callback is running on a separate thread.
+    logging.info('Starting stream...')
+    audio_queue = queue.Queue()
+    callback = partial(stream_callback, audio_queue=audio_queue)
+    with sd.InputStream(samplerate=FLAGS.sample_rate,
+                        blocksize=block_size,
+                        device=FLAGS.input_device,
+                        channels=FLAGS.num_channels,
+                        dtype=np.float32,
+                        latency=FLAGS.latency,
+                        callback=callback):
+        while True:
+            # Process chunks of audio from the queue.
+            process_audio(audio_queue, model)
 
 
 if __name__ == '__main__':
